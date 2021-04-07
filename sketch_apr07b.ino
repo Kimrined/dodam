ee6881cdb4a79a3232b175a56df3526648089d78
eunji <yuki012398@naver.com>
2021-04-07 22:25:23 +0900

    Turbidity sensor

--- sketch_apr07b.ino
+++ sketch_apr07b.ino
@@ -1,9 +1,9 @@
 void setup() {
-  Serial.begin(9600); //Baud rate: 9600
+  Serial.begin(9600);
 }
 void loop() {
-  int sensorValue = analogRead(A0);// read the input on analog pin 0:
-  float voltage = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  Serial.println(voltage); // print out the value you read:
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1024.0); // 0~5V 까지는 0~1023 수치로 나타냄
+  Serial.println(voltage);
   delay(500);
 }
